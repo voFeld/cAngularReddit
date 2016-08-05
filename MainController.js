e871977f7994a2fa2fde211d6ecc50af4c81cf73
voFeld <vofeld@gmail.com>
2016-08-05 19:23:34 +0300

    finished the main exercise

---
+++ MainController.js
@@ -0,0 +1,23 @@
+app.controller('MainController', function($scope) {
+    $scope.postsArray = [];
+    $scope.votes = 0;
+
+    $scope.createPost = function(userInput){
+
+        post = {
+            text: userInput,
+            votes: 0
+        };
+
+        $scope.postsArray.unshift(post);
+    };
+
+    $scope.voteUp = function(){
+        this.post.votes += 1;
+    };
+
+
+    for(var i = 0; i < $scope.postsArray.length; i++) {
+
+    };
+});
